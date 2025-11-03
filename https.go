c8a101253479ca9fde0597fa912a3828be1b38b6
Ujj <usarin@gmail.com>
2025-11-03 12:32:49 -0800

    removing deletion of content-length

--- https.go
+++ https.go
@@ -407,8 +407,8 @@ func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request
 					} else {
 						// Since we don't know the length of resp, return chunked encoded response
 						// TODO: use a more reasonable scheme
-						resp.Header.Del("Content-Length")
-						resp.Header.Set("Transfer-Encoding", "chunked")
+						// resp.Header.Del("Content-Length")
+						// resp.Header.Set("Transfer-Encoding", "chunked")
 					}
 					// Force connection close otherwise chrome will keep CONNECT tunnel open forever
 					if !isWebsocket {
